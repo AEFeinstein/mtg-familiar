f112f01956353b0527286e3dbff0c6e0cb8ecdd8
AEFeinstein <3urdnot6>
2014-05-15 07:33:45 -0400

    More comprehensive exception handling

--- MTGFamiliar/src/main/java/com/gelakinetic/mtgfam/helpers/AutocompleteCursorAdapter.java
+++ MTGFamiliar/src/main/java/com/gelakinetic/mtgfam/helpers/AutocompleteCursorAdapter.java
@@ -21,7 +21,6 @@ package com.gelakinetic.mtgfam.helpers;
 
 import android.app.SearchManager;
 import android.database.Cursor;
-import android.database.CursorIndexOutOfBoundsException;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
