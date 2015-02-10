78c615d649712b41284cb6a053ea37648946313a
XVicarious <brian.maurer.42@gmail.com>
2015-02-09 23:42:49 -0500

    Removed SharedPreferences import

--- MTGFamiliar/src/main/java/com/gelakinetic/mtgfam/fragments/WishlistFragment.java
+++ MTGFamiliar/src/main/java/com/gelakinetic/mtgfam/fragments/WishlistFragment.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
