bf9363e101feb661c8e96a3bae54fd91789a194e
Adam <gelakinetic@gmail.com>
2015-08-21 00:08:13 -0400

    Removed folders left over from wear development

--- familiarconstants/src/main/java/com/gelakinetic/mtgfam/FamiliarConstants.java
+++
@@ -1,9 +0,0 @@
-package com.gelakinetic.mtgfam;
-
-public class FamiliarConstants {
-    public static final String PATH = "/RoundTimerPath";
-    public static final String KEY_END_TIME = "KEY_END_TIME";
-    public static final String KEY_FIVE_MINUTE_WARNING = "KEY_FIVE";
-    public static final String KEY_TEN_MINUTE_WARNING = "KEY_TEN";
-    public static final String KEY_FIFTEEN_MINUTE_WARNING = "KEY_FIFTEEN";
-}
