2e2b1e191444439c52f8419746ac67b7c510e60e
Adam <gelakinetic@gmail.com>
2016-02-03 20:40:39 -0500

    Rolled in latest database, ipg, jar, and mtr

--- mobile/src/main/java/com/gelakinetic/mtgfam/helpers/database/CardDbAdapter.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/helpers/database/CardDbAdapter.java
@@ -57,7 +57,7 @@ import java.util.zip.GZIPInputStream;
 @SuppressWarnings("JavaDoc")
 public class CardDbAdapter {
 
-    public static final int DATABASE_VERSION = 68;
+    public static final int DATABASE_VERSION = 69;
 
     public static final int STAR = -1000;
     public static final int ONE_PLUS_STAR = -1001;
