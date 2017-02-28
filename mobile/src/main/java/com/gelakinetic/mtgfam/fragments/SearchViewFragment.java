1f93e6a833f1542c2f1fb9c4e3bf60617d5e9554
xvicarious <bmaurer@xvicario.us>
2017-02-28 10:56:52 -0500

    Added Adapters to center the text in certain spinners in the search view Addresses part 1 of #172

--- mobile/src/main/java/com/gelakinetic/mtgfam/fragments/SearchViewFragment.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/fragments/SearchViewFragment.java
@@ -215,6 +215,20 @@ public class SearchViewFragment extends FamiliarFragment {
         mCmcLogic = (Spinner) myFragmentView.findViewById(R.id.cmcLogic);
         mCmcChoice = (Spinner) myFragmentView.findViewById(R.id.cmcChoice);
 
+        /* Now we need to apply a different TextView to our Spinners to center the items */
+        ArrayAdapter<String> logicAdapter = new ArrayAdapter<>(getContext(), R.layout.centered_spinner_text, getResources().getStringArray(R.array.logic_spinner));
+        logicAdapter.setDropDownViewResource(R.layout.centered_spinner_text);
+        mPowLogic.setAdapter(logicAdapter);
+        mTouLogic.setAdapter(logicAdapter);
+        mCmcLogic.setAdapter(logicAdapter);
+        ArrayAdapter<String> ptChoiceAdapter = new ArrayAdapter<>(getContext(), R.layout.centered_spinner_text, getResources().getStringArray(R.array.pt_spinner));
+        ptChoiceAdapter.setDropDownViewResource(R.layout.centered_spinner_text);
+        mPowChoice.setAdapter(ptChoiceAdapter);
+        mTouChoice.setAdapter(ptChoiceAdapter);
+        ArrayAdapter<String> cmcChoiceAdapter = new ArrayAdapter<>(getContext(), R.layout.centered_spinner_text, getResources().getStringArray(R.array.cmc_spinner));
+        cmcChoiceAdapter.setDropDownViewResource(R.layout.centered_spinner_text);
+        mCmcChoice.setAdapter(cmcChoiceAdapter);
+
         /* set the buttons to open the dialogs */
         mSetButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
