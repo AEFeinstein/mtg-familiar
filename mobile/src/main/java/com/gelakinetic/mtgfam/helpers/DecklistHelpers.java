c6e3e8dfbd80403dc1e4e8b23eb95bef9e075685
xvicarious <bmaurer@xvicario.us>
2017-03-01 13:41:30 -0500

    sectioned out compare to remove as much redundant code as possible

--- mobile/src/main/java/com/gelakinetic/mtgfam/helpers/DecklistHelpers.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/helpers/DecklistHelpers.java
@@ -23,87 +23,81 @@ public class DecklistHelpers {
     public static class DecklistComparator implements Comparator<CompressedDecklistInfo> {
         @Override
         public int compare(CompressedDecklistInfo o1, CompressedDecklistInfo o2) {
-            /*
-            Creature
-            Spells (Instant/Sorcery)
-            Artifact
-            Enchantment
-            Land
-             */
-            int nameCompare = 0;
-            if (o1.mIsSideboard && !o2.mIsSideboard) { // >
+            int cmcCompare;
+            if (o1.mIsSideboard && !o2.mIsSideboard) {
                 return 1;
-            } else if (!o1.mIsSideboard && o2.mIsSideboard) { // <
+            } else if (!o1.mIsSideboard && o2.mIsSideboard) {
                 return -1;
             } else {
                 if (o1.mCard.type.contains("Creature") && !o2.mCard.type.contains("Creature")) {
                     return -1;
                 } else if (o1.mCard.type.contains("Creature") && o2.mCard.type.contains("Creature")) {
-                    if (o1.mCard.cmc < o2.mCard.cmc) {
-                        return -1;
-                    } else if (o1.mCard.cmc > o2.mCard.cmc) {
-                        return 1;
-                    } else {
-                        nameCompare = o1.mCard.name.compareTo(o2.mCard.name);
-                        if (nameCompare > 0) {
-                            return 1;
-                        } else if (nameCompare < 0) {
-                            return -1;
-                        }
+                    cmcCompare = compareCMC(o1.mCard.cmc, o2.mCard.cmc);
+                    if (cmcCompare == 0) {
+                        return compareName(o1.mCard.name.compareTo(o2.mCard.name));
                     }
-                } else if ((o1.mCard.type.contains("Instant") || o1.mCard.type.contains("Sorcery")) && !(o2.mCard.type.contains("Instant") || o2.mCard.type.contains("Sorcery"))) {
+                    return cmcCompare;
+                } else if (o1.mCard.type.contains("Planeswalker") && !o2.mCard.type.contains("Planeswalker")) {
+                    if (o2.mCard.type.contains("Creature")) {
+                        return 1;
+                    }
+                    return -1;
+                } else if ((o1.mCard.type.contains("Instant") || o1.mCard.type.contains("Sorcery")) && !(o2.mCard.type.contains("Instant") && o2.mCard.type.contains("Sorcery"))) {
+                    if (o2.mCard.type.contains("Creature") || o2.mCard.type.contains("Planeswalker")) {
+                        return 1;
+                    }
                     return -1;
                 } else if ((o1.mCard.type.contains("Instant") || o1.mCard.type.contains("Sorcery")) && (o2.mCard.type.contains("Instant") || o2.mCard.type.contains("Sorcery"))) {
-                    if (o1.mCard.cmc < o2.mCard.cmc) {
-                        return -1;
-                    } else if (o1.mCard.cmc > o2.mCard.cmc) {
-                        return 1;
-                    } else {
-                        nameCompare = o1.mCard.name.compareTo(o2.mCard.name);
-                        if (nameCompare > 0) {
-                            return 1;
-                        } else if (nameCompare < 0) {
-                            return -1;
-                        }
+                    cmcCompare = compareCMC(o1.mCard.cmc, o2.mCard.cmc);
+                    if (cmcCompare == 0) {
+                        return compareName(o1.mCard.name.compareTo(o2.mCard.name));
                     }
+                    return cmcCompare;
                 } else if (o1.mCard.type.contains("Artifact") && !o2.mCard.type.contains("Artifact")) {
+                    if (o2.mCard.type.contains("Creature") || o2.mCard.type.contains("Planeswalker") || o2.mCard.type.contains("Instant") || o2.mCard.type.contains("Sorcery")) {
+                        return 1;
+                    }
                     return -1;
                 } else if (o1.mCard.type.contains("Artifact") && o2.mCard.type.contains("Artifact")) {
-                    if (o1.mCard.cmc < o2.mCard.cmc) {
-                        return -1;
-                    } else if (o1.mCard.cmc > o2.mCard.cmc) {
-                        return 1;
-                    } else {
-                        nameCompare = o1.mCard.name.compareTo(o2.mCard.name);
-                        if (nameCompare > 0) {
-                            return 1;
-                        } else if (nameCompare < 0) {
-                            return -1;
-                        }
+                    cmcCompare = compareCMC(o1.mCard.cmc, o2.mCard.cmc);
+                    if (cmcCompare == 0) {
+                        return compareName(o1.mCard.name.compareTo(o2.mCard.name));
                     }
                 } else if (o1.mCard.type.contains("Enchantment") && !o2.mCard.type.contains("Enchantment")) {
+                    if (o2.mCard.type.contains("Creature") || o2.mCard.type.contains("Planeswalker") || o2.mCard.type.contains("Instant") || o2.mCard.type.contains("Sorcery") || o2.mCard.type.contains("Artifact")) {
+                        return 1;
+                    }
                     return -1;
                 } else if (o1.mCard.type.contains("Enchantment") && o2.mCard.type.contains("Enchantment")) {
-                    if (o1.mCard.cmc < o2.mCard.cmc) {
-                        return -1;
-                    } else if (o1.mCard.cmc > o2.mCard.cmc) {
-                        return 1;
-                    } else {
-                        nameCompare = o1.mCard.name.compareTo(o2.mCard.name);
-                        if (nameCompare > 0) {
-                            return -1;
-                        } else if (nameCompare < 0) {
-                            return 1;
-                        }
+                    cmcCompare = compareCMC(o1.mCard.cmc, o2.mCard.cmc);
+                    if (cmcCompare == 0) {
+                        return compareName(o1.mCard.name.compareTo(o2.mCard.name));
                     }
+                    return cmcCompare;
                 } else {
-                    nameCompare = o1.mCard.name.compareTo(o2.mCard.name);
-                    if (nameCompare > 0) {
+                    if (o2.mCard.type.contains("Creature") || o2.mCard.type.contains("Planeswalker") || o2.mCard.type.contains("Instant") || o2.mCard.type.contains("Sorcery") || o2.mCard.type.contains("Artifact") || o2.mCard.type.contains("Enchantment")) {
                         return 1;
-                    } else if (nameCompare < 0) {
-                        return -1;
                     }
+                    return compareName(o1.mCard.name.compareTo(o2.mCard.name));
                 }
+            }
+            return 0;
+        }
+
+        private int compareCMC(int cmcValue1, int cmcValue2) {
+            if (cmcValue1 < cmcValue2) {
+                return -1;
+            } else if (cmcValue1 > cmcValue2) {
+                return 1;
+            }
+            return 0;
+        }
+
+        private int compareName(int nameValue) {
+            if (nameValue > 0) {
+                return 1;
+            } else if (nameValue < 0) {
+                return -1;
             }
             return 0;
         }
