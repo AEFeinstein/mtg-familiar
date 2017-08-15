e53ae4e75a02a368417be0dc91438dbf17960443
MarcelBucz <marcel.buczkiewicz@gmail.com>
2017-08-15 22:52:54 +0200

    Initial

--- mobile/src/main/java/com/gelakinetic/mtgfam/helpers/view/ATokenTextView.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/helpers/view/ATokenTextView.java
@@ -8,6 +8,33 @@ import android.view.KeyEvent;
 import com.tokenautocomplete.TokenCompleteTextView;
 
 public abstract class ATokenTextView extends TokenCompleteTextView<String> {
+    private class TokenListener implements TokenCompleteTextView.TokenListener<String> {
+        private TokenCompleteTextView.TokenListener<String> tokenListener;
+
+        @Override
+        public void onTokenAdded(String token) {
+            if (this.tokenListener != null) {
+                tokenListener.onTokenAdded(token);
+            }
+        }
+
+        @Override
+        public void onTokenRemoved(String token) {
+            if (ATokenTextView.this.getObjects().isEmpty()) {
+                ATokenTextView.this.onRestoreInstanceState(
+                        ATokenTextView.this.onSaveInstanceState());
+            }
+            if (this.tokenListener != null) {
+                tokenListener.onTokenRemoved(token);
+            }
+        }
+
+        private void setTokenListener(TokenCompleteTextView.TokenListener<String> tokenListener) {
+            this.tokenListener = tokenListener;
+        }
+    }
+
+    private ATokenTextView.TokenListener tokenListenerWrapper = new ATokenTextView.TokenListener();
     private boolean handleDismiss = false;
 
     public ATokenTextView(Context context, AttributeSet attrs) {
@@ -15,11 +42,12 @@ public abstract class ATokenTextView extends TokenCompleteTextView<String> {
         this.performBestGuess(false);
         this.allowCollapse(true);
         this.setTokenClickStyle(TokenClickStyle.Delete);
+        super.setTokenListener(tokenListenerWrapper);
     }
 
     @Override
     protected String defaultObject(String completionText) {
-        return "";
+        return " ";
     }
 
     @Override
@@ -62,6 +90,21 @@ public abstract class ATokenTextView extends TokenCompleteTextView<String> {
     public void dismissDropDown() {
         if (handleDismiss) {
             super.dismissDropDown();
+        }
+    }
+
+    @Override
+    public void setTokenListener(TokenCompleteTextView.TokenListener<String> l) {
+        tokenListenerWrapper.setTokenListener(l);
+    }
+
+    public void clearTextAndTokens() {
+        if (this.getObjects().isEmpty()) {
+            this.onRestoreInstanceState(this.onSaveInstanceState());
+        } else {
+            for (String token : this.getObjects()) {
+                this.removeObject(token);
+            }
         }
     }
 }
