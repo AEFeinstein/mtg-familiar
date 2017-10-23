425ba65b722409780aace461d345ffdbca0d3bdc
Adam <gelakinetic@gmail.com>
2017-10-22 21:20:57 -0400

    Reworked clearTextAndTokens Now that the src for tokenautocomplete is in Familiar, add a function which properly clears all objects and stray text. Removed the kludgy wrapper which we had been using for clearing fields. clearTextAndTokens() is modeled on removeObject(), but for all objects, and adds code to clear stray text too.

--- mobile/src/main/java/com/gelakinetic/mtgfam/helpers/view/ATokenTextView.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/helpers/view/ATokenTextView.java
@@ -2,42 +2,13 @@ package com.gelakinetic.mtgfam.helpers.view;
 
 import android.content.Context;
 import android.graphics.Rect;
-import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 
 import com.tokenautocomplete.TokenCompleteTextView;
 
 public abstract class ATokenTextView extends TokenCompleteTextView<String> {
-    private class TokenListener implements TokenCompleteTextView.TokenListener<String> {
-        private TokenCompleteTextView.TokenListener<String> tokenListener;
 
-        @Override
-        public void onTokenAdded(String token) {
-            if (this.tokenListener != null) {
-                tokenListener.onTokenAdded(token);
-            }
-        }
-
-        @Override
-        public void onTokenRemoved(String token) {
-            if (ATokenTextView.this.getObjects().isEmpty()) {
-                String value = ATokenTextView.this.getText().toString();
-                if (hasNonSplitChar(value)) {
-                    ATokenTextView.this.clearText();
-                }
-            }
-            if (this.tokenListener != null) {
-                tokenListener.onTokenRemoved(token);
-            }
-        }
-
-        private void setTokenListener(TokenCompleteTextView.TokenListener<String> tokenListener) {
-            this.tokenListener = tokenListener;
-        }
-    }
-
-    private final ATokenTextView.TokenListener tokenListenerWrapper = new ATokenTextView.TokenListener();
     private boolean handleDismiss = false;
 
     public ATokenTextView(Context context, AttributeSet attrs) {
@@ -45,7 +16,6 @@ public abstract class ATokenTextView extends TokenCompleteTextView<String> {
         this.performBestGuess(false);
         this.allowCollapse(true);
         this.setTokenClickStyle(TokenClickStyle.Delete);
-        super.setTokenListener(tokenListenerWrapper);
     }
 
     @Override
@@ -94,36 +64,5 @@ public abstract class ATokenTextView extends TokenCompleteTextView<String> {
         if (handleDismiss) {
             super.dismissDropDown();
         }
-    }
-
-    @Override
-    public void setTokenListener(TokenCompleteTextView.TokenListener<String> l) {
-        tokenListenerWrapper.setTokenListener(l);
-    }
-
-    public void clearTextAndTokens() {
-        if (this.getObjects().isEmpty()) {
-            this.clearText();
-        } else {
-            for (String token : this.getObjects()) {
-                this.removeObject(token);
-            }
-        }
-    }
-
-    private boolean hasNonSplitChar(String s) {
-        for(char c : s.toCharArray()){
-            // These two chars are used for splitting tokens in TokenCompleteTextView
-            // Unfortunately the array splitChar[] is private there.
-            if(c != ',' && c != ';') {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void clearText() {
-        final Parcelable state = this.onSaveInstanceState();
-        this.onRestoreInstanceState(state);
     }
 }
