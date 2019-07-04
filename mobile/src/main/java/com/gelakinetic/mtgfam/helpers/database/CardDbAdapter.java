ed490b99f59fb9a1634822ba22adadd1caa4a732
gelakinetic <gelakinetic@gmail.com>
2019-07-04 10:08:25 -0400

    Update database from expansion-symbols branch
    The src was temporarily modified to use these patches
    https://github.com/AEFeinstein/GathererScraper/tree/expansion-symbols

--- mobile/src/main/java/com/gelakinetic/mtgfam/helpers/database/CardDbAdapter.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/helpers/database/CardDbAdapter.java
@@ -51,7 +51,7 @@ import java.util.Set;
 public class CardDbAdapter {
 
     /* Database version. Must be incremented whenever datagz is updated */
-    public static final int DATABASE_VERSION = 103;
+    public static final int DATABASE_VERSION = 104;
 
     /* Database Tables */
     public static final String DATABASE_TABLE_CARDS = "cards";
