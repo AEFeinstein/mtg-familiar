7c0d54b86988fe88b0a0c1fe88c00ae7ecab17e6
gelakinetic <gelakinetic@gmail.com>
2020-11-29 01:18:48 -0500

    Switch all urls to Mtgjson2Familiar

--- mobile/src/main/java/com/gelakinetic/mtgfam/helpers/updaters/MTRIPGParser.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/helpers/updaters/MTRIPGParser.java
@@ -48,15 +48,15 @@ class MTRIPGParser {
     public static final int MODE_DIPG = 3;
     public static final int MODE_DMTR = 4;
     private static final String MTR_SOURCE =
-            "https://raw.githubusercontent.com/AEFeinstein/GathererScraper/master/rules/MagicTournamentRules-light.html";
+            "https://github.com/AEFeinstein/Mtgjson2Familiar/raw/main/rules/MagicTournamentRules-light.html";
     private static final String IPG_SOURCE =
-            "https://raw.githubusercontent.com/AEFeinstein/GathererScraper/master/rules/InfractionProcedureGuide-light.html";
+            "https://github.com/AEFeinstein/Mtgjson2Familiar/raw/main/rules/InfractionProcedureGuide-light.html";
     private static final String JAR_SOURCE =
-            "https://raw.githubusercontent.com/AEFeinstein/GathererScraper/master/rules/JudgingAtRegular-light.html";
+            "https://github.com/AEFeinstein/Mtgjson2Familiar/raw/main/rules/JudgingAtRegular-light.html";
     private static final String DMTR_SOURCE =
-            "https://raw.githubusercontent.com/AEFeinstein/GathererScraper/master/rules/DigitalTournamentRules-light.html";
+            "https://github.com/AEFeinstein/Mtgjson2Familiar/raw/main/rules/DigitalTournamentRules-light.html";
     private static final String DIPG_SOURCE =
-            "https://raw.githubusercontent.com/AEFeinstein/GathererScraper/master/rules/DigitalInfractionProcedureGuide-light.html";
+            "https://github.com/AEFeinstein/Mtgjson2Familiar/raw/main/rules/DigitalInfractionProcedureGuide-light.html";
     private static final String MTR_LOCAL_FILE = "MTR.html";
     private static final String IPG_LOCAL_FILE = "IPG.html";
     private static final String JAR_LOCAL_FILE = "JAR.html";
