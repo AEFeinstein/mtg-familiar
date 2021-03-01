673ed2faf9d4a8707b0a031e06ff17f56e73f473
Evan R <70585539+edrutte@users.noreply.github.com>
2021-03-01 11:33:54 -0500

    Create constructor for fragment

--- mobile/src/main/java/com/gelakinetic/mtgfam/fragments/DeckStatsFragment.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/fragments/DeckStatsFragment.java
@@ -1,13 +1,28 @@
 package com.gelakinetic.mtgfam.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import com.gelakinetic.mtgfam.helpers.DeckStatsGenerator;
+import com.gelakinetic.mtgfam.helpers.MtgCard;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public class DeckStatsFragment extends FamiliarFragment {
+    private DeckStatsGenerator mStatGenerator;
+    private List<MtgCard> mDeckToStat;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public DeckStatsFragment (List<MtgCard> mDeckToStat) {
+        this.mDeckToStat = mDeckToStat;
+        mStatGenerator = new DeckStatsGenerator(mDeckToStat);
+    }
+
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return null;
