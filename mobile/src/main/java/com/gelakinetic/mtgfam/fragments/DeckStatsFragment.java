04b9c09fcab1a4f2c65cd6180e10d7a967ae4b24
Evan R <70585539+edrutte@users.noreply.github.com>
2021-03-01 14:23:21 -0500

    Start work on making graphs

--- mobile/src/main/java/com/gelakinetic/mtgfam/fragments/DeckStatsFragment.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/fragments/DeckStatsFragment.java
@@ -7,8 +7,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+
+import com.gelakinetic.mtgfam.R;
 import com.gelakinetic.mtgfam.helpers.DeckStatsGenerator;
 import com.gelakinetic.mtgfam.helpers.MtgCard;
+import com.github.mikephil.charting.charts.PieChart;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -16,6 +20,8 @@ import java.util.List;
 public class DeckStatsFragment extends FamiliarFragment {
     private DeckStatsGenerator mStatGenerator;
     private List<MtgCard> mDeckToStat;
+    private PieChart mTypeChart;
+    private PieChart mColorChart;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public DeckStatsFragment (List<MtgCard> mDeckToStat) {
@@ -25,6 +31,10 @@ public class DeckStatsFragment extends FamiliarFragment {
 
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return null;
+        final View myFragmentView =
+                inflater.inflate(R.layout.stat_frag, container, false);
+        assert myFragmentView != null;
+        mTypeChart = (PieChart) myFragmentView.findViewById(R.id.chart);
+        return myFragmentView;
     }
 }
--- mobile/src/main/java/com/gelakinetic/mtgfam/helpers/DeckStatsGenerator.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/helpers/DeckStatsGenerator.java
@@ -11,48 +11,57 @@ import java.util.regex.Pattern;
 
 public class DeckStatsGenerator {
     private List<MtgCard> mDeckToStat;
-    private Map<String, Integer> typeStats;
-    private Map<String, Integer> colorStats;
-    private Map<Integer, Integer> cmcStats;
+    private Float mDeckSize;
+    private Map<String, Float> typeStats;
+    private Map<String, Float> colorStats;
+    private Map<Integer, Float> cmcStats;
     private static final Pattern mTypePattern = Pattern.compile("(Land|Creature|Planeswalker|Instant|Sorcery|Artifact|Enchantment)");
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public DeckStatsGenerator(List<MtgCard> mDeckToStat) {
         this.mDeckToStat = mDeckToStat;
+        mDeckSize = (float) mDeckToStat.size();
         runStats();
     }
     private void resetStats() {
         typeStats = new HashMap<>();
-        typeStats.put("Creature", 0);
-        typeStats.put("Planeswalker", 0);
-        typeStats.put("Instant", 0);
-        typeStats.put("Sorcery", 0);
-        typeStats.put("Artifact", 0);
-        typeStats.put("Enchantment", 0);
-        typeStats.put("Land", 0);
+        typeStats.put("Creature", (float) 0);
+        typeStats.put("Planeswalker", (float) 0);
+        typeStats.put("Instant", (float) 0);
+        typeStats.put("Sorcery", (float) 0);
+        typeStats.put("Artifact", (float) 0);
+        typeStats.put("Enchantment", (float) 0);
+        typeStats.put("Land", (float) 0);
         colorStats = new HashMap<>();
-        colorStats.put("W", 0);
-        colorStats.put("U", 0);
-        colorStats.put("B", 0);
-        colorStats.put("R", 0);
-        colorStats.put("G", 0);
-        colorStats.put("", 0);
+        colorStats.put("W", (float) 0);
+        colorStats.put("U", (float) 0);
+        colorStats.put("B", (float) 0);
+        colorStats.put("R", (float) 0);
+        colorStats.put("G", (float) 0);
+        colorStats.put("", (float) 0);
         cmcStats = new HashMap<>();
-        cmcStats.put(0, 0);
-        cmcStats.put(1, 0);
-        cmcStats.put(2, 0);
-        cmcStats.put(3, 0);
-        cmcStats.put(4, 0);
-        cmcStats.put(5, 0);
-        cmcStats.put(6, 0);
-        cmcStats.put(7, 0);
+        cmcStats.put(0, (float) 0);
+        cmcStats.put(1, (float) 0);
+        cmcStats.put(2, (float) 0);
+        cmcStats.put(3, (float) 0);
+        cmcStats.put(4, (float) 0);
+        cmcStats.put(5, (float) 0);
+        cmcStats.put(6, (float) 0);
+        cmcStats.put(7, (float) 0);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public Map[] runStats() {
+    private void runStats() {
         resetStats();
         for (MtgCard card : mDeckToStat) {
-            colorStats.computeIfPresent(card.getColor(), (k, v) -> (v++));
+            if (!card.getColor().isEmpty()) {
+                for (String color : card.getColor().split("//B", 5)) {
+                    colorStats.computeIfPresent(color, (k, v) -> (v++));
+                }
+            } else {
+                //Catch colorless
+                colorStats.computeIfPresent(card.getColor(), (k, v) -> (v++));
+            }
             cmcStats.computeIfPresent(Math.min(card.getCmc(), 7), (k, v) -> (v++));
             Matcher match = mTypePattern.matcher(card.getType());
             //Must have at least 1 type
@@ -62,7 +71,14 @@ public class DeckStatsGenerator {
             } //Can have more than 1 type
             while (match.find());
         }
-        return new Map[]{typeStats, colorStats, cmcStats};
+        for (Map<String, Float> statMap : new Map<String, Float>[]{typeStats, colorStats}) {
+            for (String key : statMap.keySet()) {
+                statMap.compute(key, (k, v) -> (v / mDeckSize));
+            }
+        }
+        for (Integer cmc : cmcStats.keySet()) {
+            cmcStats.compute(cmc, (k, v) -> (v / mDeckSize));
+        }
     }
 
     public Map[] getStats() {
