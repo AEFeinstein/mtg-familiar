a9502c19d41abfbf96af0aeae170a7c373329b70
Evan R <70585539+edrutte@users.noreply.github.com>
2021-03-04 13:40:28 -0500

    Create methods for getting sample hands

--- mobile/src/main/java/com/gelakinetic/mtgfam/helpers/SampleHandMaker.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/helpers/SampleHandMaker.java
@@ -2,15 +2,28 @@ package com.gelakinetic.mtgfam.helpers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class SampleHandMaker {
 
-    public static List<MtgCard> drawSampleHand(List<MtgCard> mFullDeck) {
-        return null;
+    public static List<MtgCard> drawSampleHand(List<MtgCard> mDeck) {
+        ArrayList<MtgCard> sampleHand = new ArrayList<>();
+        List<MtgCard> fullDeck = expandDeck(mDeck);
+        Random rand = new Random();
+        for (int i = 0; i < 7; i++) {
+            int randCard = rand.nextInt(fullDeck.size());
+            sampleHand.add(fullDeck.get(randCard));
+            fullDeck.remove(randCard);
+        }
+        return sampleHand;
     }
 
-    public static List<MtgCard> drawMulligan(List<MtgCard> mFullDeck, int numOfMulls) {
-        return null;
+    public static List<MtgCard> drawSampleHand(List<MtgCard> mDeck, int numOfMulls) {
+        List<MtgCard> sampleHand = drawSampleHand(mDeck);
+        for (int i = 0; i < numOfMulls; i++) {
+            sampleHand.remove(6 - i);
+        }
+        return sampleHand;
     }
 
     private static List<MtgCard> expandDeck(List<MtgCard> mDeck) {
