5536000c8760b728e144131585e720650c7342a5
Evan R <70585539+edrutte@users.noreply.github.com>
2021-03-04 13:03:21 -0500

    Stub out samplehand_frag.xml with an empty LinearLayout and add copyright notice to xmls

--- mobile/src/main/java/com/gelakinetic/mtgfam/fragments/SampleHandFrag.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/fragments/SampleHandFrag.java
@@ -7,9 +7,14 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 
+import com.gelakinetic.mtgfam.R;
+
 public class SampleHandFrag extends FamiliarFragment{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return null;
+        final View myFragmentView =
+                inflater.inflate(R.layout.samplehand_frag, container, false);
+        assert myFragmentView != null;
+        return myFragmentView;
     }
 }
