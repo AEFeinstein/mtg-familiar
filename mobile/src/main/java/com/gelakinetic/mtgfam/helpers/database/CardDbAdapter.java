b5367bcce3be70ad93538a1803a95260855c5802
gelakinetic <gelakinetic@gmail.com>
2021-04-18 22:57:02 -0400

    #369 #536 Sort color and rarity more accurately

--- mobile/src/main/java/com/gelakinetic/mtgfam/helpers/database/CardDbAdapter.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/helpers/database/CardDbAdapter.java
@@ -1350,8 +1350,113 @@ public class CardDbAdapter {
                 orderByStr = KEY_NAME + " COLLATE UNICODE";
             } else if (!(criteria.setLogic != MOST_RECENT_PRINTING && criteria.setLogic != ALL_PRINTINGS)) {
                 // When sorting by set, sort by date first, then name
-                orderByStr = orderByStr.replace(CardDbAdapter.KEY_SET + " asc", CardDbAdapter.KEY_DATE + " asc," + CardDbAdapter.KEY_SET + " asc");
-                orderByStr = orderByStr.replace(CardDbAdapter.KEY_SET + " desc", CardDbAdapter.KEY_DATE + " desc," + CardDbAdapter.KEY_SET + " desc");
+                orderByStr = orderByStr.replace(
+                        CardDbAdapter.KEY_SET + " asc",
+                        CardDbAdapter.KEY_DATE + " asc," + CardDbAdapter.KEY_SET + " asc");
+                orderByStr = orderByStr.replace(
+                        CardDbAdapter.KEY_SET + " desc",
+                        CardDbAdapter.KEY_DATE + " desc," + CardDbAdapter.KEY_SET + " desc");
+
+                // Sort rarity by C, U, R, M, T order, not 'alphabetically'
+                orderByStr = orderByStr.replace(CardDbAdapter.KEY_RARITY + " asc",
+                        "CASE " + CardDbAdapter.KEY_RARITY +
+                                " WHEN " + ((int) 'C') + " THEN 0" +
+                                " WHEN " + ((int) 'U') + " THEN 1" +
+                                " WHEN " + ((int) 'R') + " THEN 2" +
+                                " WHEN " + ((int) 'M') + " THEN 3" +
+                                " WHEN " + ((int) 'T') + " THEN 4" +
+                                " ELSE                          5" +
+                                " END");
+                orderByStr = orderByStr.replace(CardDbAdapter.KEY_RARITY + " desc",
+                        "CASE " + CardDbAdapter.KEY_RARITY +
+                                " WHEN " + ((int) 'C') + " THEN 4" +
+                                " WHEN " + ((int) 'U') + " THEN 3" +
+                                " WHEN " + ((int) 'R') + " THEN 2" +
+                                " WHEN " + ((int) 'M') + " THEN 1" +
+                                " WHEN " + ((int) 'T') + " THEN 0" +
+                                " ELSE                          5" +
+                                " END");
+
+                // Sort color and color identity by W, U, B, R, G order, not 'alphabetically'
+                String colorAscStrCases = " WHEN 'W'     THEN  0" +
+                        " WHEN 'U'     THEN  1" +
+                        " WHEN 'B'     THEN  2" +
+                        " WHEN 'R'     THEN  3" +
+                        " WHEN 'G'     THEN  4" +
+                        " WHEN 'WU'    THEN  5" +
+                        " WHEN 'UB'    THEN  6" +
+                        " WHEN 'BR'    THEN  7" +
+                        " WHEN 'RG'    THEN  8" +
+                        " WHEN 'WG'    THEN  9" +
+                        " WHEN 'WB'    THEN 10" +
+                        " WHEN 'BG'    THEN 11" +
+                        " WHEN 'UG'    THEN 12" +
+                        " WHEN 'UR'    THEN 13" +
+                        " WHEN 'WR'    THEN 14" +
+                        " WHEN 'WUB'   THEN 15" +
+                        " WHEN 'UBR'   THEN 16" +
+                        " WHEN 'BRG'   THEN 17" +
+                        " WHEN 'WRG'   THEN 18" +
+                        " WHEN 'WUG'   THEN 19" +
+                        " WHEN 'WUR'   THEN 20" +
+                        " WHEN 'UBG'   THEN 21" +
+                        " WHEN 'WBR'   THEN 22" +
+                        " WHEN 'URG'   THEN 23" +
+                        " WHEN 'WBG'   THEN 24" +
+                        " WHEN 'WUBR'  THEN 25" +
+                        " WHEN 'WUBG'  THEN 26" +
+                        " WHEN 'WURG'  THEN 27" +
+                        " WHEN 'WBRG'  THEN 28" +
+                        " WHEN 'UBRG'  THEN 29" +
+                        " WHEN 'WUBRG' THEN 30" +
+                        " ELSE              31" +
+                        " END";
+                String colorDescStrCases = " WHEN 'W'     THEN 31" +
+                        " WHEN 'U'     THEN 30" +
+                        " WHEN 'B'     THEN 29" +
+                        " WHEN 'R'     THEN 28" +
+                        " WHEN 'G'     THEN 27" +
+                        " WHEN 'WU'    THEN 26" +
+                        " WHEN 'UB'    THEN 25" +
+                        " WHEN 'BR'    THEN 24" +
+                        " WHEN 'RG'    THEN 23" +
+                        " WHEN 'WG'    THEN 22" +
+                        " WHEN 'WB'    THEN 21" +
+                        " WHEN 'BG'    THEN 20" +
+                        " WHEN 'UG'    THEN 19" +
+                        " WHEN 'UR'    THEN 18" +
+                        " WHEN 'WR'    THEN 17" +
+                        " WHEN 'WUB'   THEN 16" +
+                        " WHEN 'UBR'   THEN 15" +
+                        " WHEN 'BRG'   THEN 14" +
+                        " WHEN 'WRG'   THEN 13" +
+                        " WHEN 'WUG'   THEN 12" +
+                        " WHEN 'WUR'   THEN 11" +
+                        " WHEN 'UBG'   THEN 10" +
+                        " WHEN 'WBR'   THEN  9" +
+                        " WHEN 'URG'   THEN  8" +
+                        " WHEN 'WBG'   THEN  7" +
+                        " WHEN 'WUBR'  THEN  6" +
+                        " WHEN 'WUBG'  THEN  5" +
+                        " WHEN 'WURG'  THEN  4" +
+                        " WHEN 'WBRG'  THEN  3" +
+                        " WHEN 'UBRG'  THEN  2" +
+                        " WHEN 'WUBRG' THEN  1" +
+                        " ELSE               0" +
+                        " END";
+
+                orderByStr = orderByStr.replace(CardDbAdapter.KEY_COLOR + " asc",
+                        "CASE " + CardDbAdapter.KEY_COLOR + colorAscStrCases
+                );
+                orderByStr = orderByStr.replace(CardDbAdapter.KEY_COLOR + " desc",
+                        "CASE " + CardDbAdapter.KEY_COLOR + colorDescStrCases
+                );
+                orderByStr = orderByStr.replace(CardDbAdapter.KEY_COLOR_IDENTITY + " asc",
+                        "CASE " + CardDbAdapter.KEY_COLOR_IDENTITY + colorAscStrCases
+                );
+                orderByStr = orderByStr.replace(CardDbAdapter.KEY_COLOR_IDENTITY + " desc",
+                        "CASE " + CardDbAdapter.KEY_COLOR_IDENTITY + colorDescStrCases
+                );
             }
 
             if (consolidate) {
