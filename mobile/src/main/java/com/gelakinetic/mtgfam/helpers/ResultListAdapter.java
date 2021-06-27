8be9e4469bf918b97f3f7677e4c5de34b9d3cc4b
gelakinetic <gelakinetic@gmail.com>
2021-06-27 13:26:27 -0400

    #549 Catch exceptions in ResultListAdapter

--- mobile/src/main/java/com/gelakinetic/mtgfam/helpers/ResultListAdapter.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/helpers/ResultListAdapter.java
@@ -68,6 +68,48 @@ public class ResultListAdapter extends SimpleCursorAdapter {
     }
 
     /**
+     * Get a View that displays the data at the specified position in the data set. You can either
+     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
+     * parent View (GridView, ListView...) will apply default layout parameters unless you use
+     * android.view.LayoutInflater.inflate(int, ViewGroup, boolean) to specify a root view and to
+     * prevent attachment to the root.
+     *
+     * @param position    The position of the item within the adapter's data set of the item whose
+     *                    view we want.
+     * @param convertView The old view to reuse, if possible. Note: You should check that this view
+     *                    is non-null and of an appropriate type before using. If it is not possible
+     *                    to convert this view to display the correct data, this method can create a
+     *                    new view. Heterogeneous lists can specify their number of view types, so
+     *                    that this View is always of the right type (see getViewTypeCount() and
+     *                    getItemViewType(int)).
+     * @param parent      The parent that this view will eventually be attached to
+     * @return A View corresponding to the data at the specified position.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        try {
+            return super.getView(position, convertView, parent);
+        } catch (IllegalStateException e) {
+            // Something went wrong, try to get a view from somewhere (i.e. not crash)
+            View v;
+            if (null != convertView) {
+                // If it exists, use the given convertView
+                v = convertView;
+            } else if (null != parent && null != parent.getContext()) {
+                // If there is a parent that has a context, make a new dummy view
+                v = newView(parent.getContext(), null, parent);
+            } else {
+                // No view to convert and no parent. Nothing we can do here :(
+                return null;
+            }
+
+            // Hide everything in the view and return it
+            v.findViewById(R.id.card_row_full).setVisibility(View.GONE);
+            return v;
+        }
+    }
+
+    /**
      * Inflates view(s) from the specified XML file.
      *
      * @param context Interface to application's global information
