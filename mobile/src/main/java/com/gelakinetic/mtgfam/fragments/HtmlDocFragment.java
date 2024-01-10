ec0cbbda2bf5d44227c65e0832c5a187f6f6cc46
gelakinetic <gelakinetic@gmail.com>
2024-01-10 10:29:30 -0500

    Update gradle Remove unused imports Add auto-translated strings

--- mobile/src/main/java/com/gelakinetic/mtgfam/fragments/HtmlDocFragment.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/fragments/HtmlDocFragment.java
@@ -46,7 +46,6 @@ import com.gelakinetic.mtgfam.helpers.database.FamiliarDbHandle;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
--- mobile/src/main/java/com/gelakinetic/mtgfam/helpers/ZipUtils.java
+++ mobile/src/main/java/com/gelakinetic/mtgfam/helpers/ZipUtils.java
@@ -29,7 +29,6 @@ import com.gelakinetic.mtgfam.helpers.util.IOUtils;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
